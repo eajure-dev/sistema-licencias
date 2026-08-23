c6adba3418692b3690fa35ae89125d8269176979
eajure-dev <ea.jure@gmail.com>
2026-08-23 01:04:38 -0400

    Update app.py

--- Sistema_Licencias_Suscripciones/app.py
+++ Sistema_Licencias_Suscripciones/app.py
@@ -598,4 +598,5 @@ def payment_webhook():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
